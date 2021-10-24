1c721b98b6dc419ef430675075e64bc995f4a2b7
Ionescu Florin-Eugen <ionescuflorin-eugen@MacBook-Pro-Ionescu.local>
2021-10-24 14:27:58 +0300

    Integrate GraphQL into an existing Express server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const { ApolloServer, gql } = require('apollo-server-express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
@@ -9,20 +11,36 @@ const port = 9000;
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 
 const app = express();
-app.use(cors(), bodyParser.json(), expressJwt({
-  secret: jwtSecret,
-  credentialsRequired: false
-}));
+app.use(
+  cors(),
+  bodyParser.json(),
+  expressJwt({
+    secret: jwtSecret,
+    credentialsRequired: false,
+    algorithms: ['RS256']
+  })
+);
+
+// TYPE DEFINITIONS
+const typeDefs = gql(
+  fs.readFileSync('./schema.graphql', { encoding: 'utf-8' })
+);
+// RESOLVERS
+const resolvers = require('./resolvers');
+// ApolloServer Instace
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+// Inject the apollo server into the extisting express server
+apolloServer.applyMiddleware({ app, path: '/graphql' });
 
 app.post('/login', (req, res) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
   const user = db.users.list().find((user) => user.email === email);
   if (!(user && user.password === password)) {
     res.sendStatus(401);
     return;
   }
-  const token = jwt.sign({sub: user.id}, jwtSecret);
-  res.send({token});
+  const token = jwt.sign({ sub: user.id }, jwtSecret);
+  res.send({ token });
 });
 
 app.listen(port, () => console.info(`Server started on port ${port}`));
